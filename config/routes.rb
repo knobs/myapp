96470cfe8fd7e0f992c301764a2f6bca14a2c225
knobs <simonknop@gmail.com>
2016-09-27 12:31:48 +0200

    install bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  
+
+  resources :articles
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
